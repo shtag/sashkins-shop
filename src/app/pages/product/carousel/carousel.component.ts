4ff14af836e0997025aad259aaba6cd371e188cd
Vasya Kanishchev <vasyakanishchev@gmail.com>
2024-01-04 20:28:41 +0200

    merge: resolve conflict

--- src/app/pages/product/carousel/carousel.component.ts
+++ src/app/pages/product/carousel/carousel.component.ts
@@ -23,50 +23,42 @@ export class CarouselComponent implements AfterViewInit {
 
   disableSwipe = false
 
-  touchStartX = 0
-
-  disableSwipe = false
-
   ngAfterViewInit() {
     this.maxImage = this.images.nativeElement.children.length
   }
 
   nextImage() {
     let right = this.images.nativeElement.style.right;
-    nextImage() {
-      let right = this.images.nativeElement.style.right;
-      this.imageNumber = (+right.split('%').join('')) / 100 + 1
-      right = (+right.split('%').join('') + 100).toString() + '%'
-      if (this.imageNumber >= this.maxImage) right = '0%'
-      this.images.nativeElement.style.right = right
-      this.images.nativeElement.style.right = right
+    this.imageNumber = (+right.split('%').join('')) / 100 + 1
+    right = (+right.split('%').join('') + 100).toString() + '%'
+    if (this.imageNumber >= this.maxImage) right = '0%'
+    this.images.nativeElement.style.right = right
+    this.images.nativeElement.style.right = right
+  }
+
+  prevImage() {
+    let right = this.images.nativeElement.style.right;
+    this.imageNumber = (+right.split('%').join('')) / 100 + 1
+    right = (+right.split('%').join('') - 100).toString() + '%'
+    if (this.imageNumber <= 1) right = ((this.maxImage - 1) * 100) + '%'
+    this.images.nativeElement.style.right = right
+  }
+
+  detectTouch(event: TouchEvent) {
+    this.touchStartX = event.changedTouches[0].screenX
+    this.disableSwipe = false
+  }
+
+  changeImage(event: TouchEvent) {
+    if (Math.abs(this.touchStartX - event.changedTouches[0].screenX) > 40 && !this.disableSwipe) {
+
+      if (this.touchStartX > event.changedTouches[0].screenX) this.nextImage()
+      else this.prevImage()
+      this.disableSwipe = true
     }
+  }
 
-    prevImage() {
-      let right = this.images.nativeElement.style.right;
-      prevImage() {
-        let right = this.images.nativeElement.style.right;
-        this.imageNumber = (+right.split('%').join('')) / 100 + 1
-        right = (+right.split('%').join('') - 100).toString() + '%'
-        if (this.imageNumber <= 1) right = ((this.maxImage - 1) * 100) + '%'
-        this.images.nativeElement.style.right = right
-      }
-
-      detectTouch(event: TouchEvent) {
-        this.touchStartX = event.changedTouches[0].screenX
-        this.disableSwipe = false
-      }
-
-      changeImage(event: TouchEvent) {
-        if (Math.abs(this.touchStartX - event.changedTouches[0].screenX) > 40 && !this.disableSwipe) {
-
-          if (this.touchStartX > event.changedTouches[0].screenX) this.nextImage()
-          else this.prevImage()
-          this.disableSwipe = true
-        }
-      }
-
-      openImage(id: number) {
-        console.log(id)
-      }
-    }
+  openImage(id: number) {
+    console.log(id)
+  }
+}
